3258f82e686c5ac7b64b2ba188cd3536a49be78b
mentorbibaj <mentorbibaj@gmail.com>
2021-02-26 23:51:04 +0100

    Fixing laravel routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,22 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/register', function () {
+    return view('register');
+});
+
+Route::get('/register', function () {
+    return view('register');
+});
+
+Route::get('/todo', function () {
+    return view('todo');
+});
